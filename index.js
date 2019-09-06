08db7f46c13b3c37f985d89bb9efb53fe4edbb3e
Sarthak Agarwal <sarthak.agarwal97@gmail.com>
2019-09-07 03:43:44 +0530

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const http = require('http');
 const fs = require('fs');
-const bodyParser=require('body-parser');
+const bodyParser=require('body-parser'); //to get the conentent of body of the request
 
 const port = 3000;
 const hostname = 'localhost';
-
 const app = express();
-app.use(bodyParser.json);
-app.use((req,res,next) => {
-    console.log(req.url);
+
+app.use(bodyPatser.json());
+
+app.all('./dishes',(req,res,next) =>{
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>HELLO</h1></body></html>');
-});
+    next();
+})
+app.get('./dishes',(req,res,next) => {
+    console.log("here the deatils are");
+})
+app.post('./dishes',(req,res,next) => {
+    console.log("here the deatils are" + req.body.name +'done' );
+})
+
+
 
 const server = http.createServer(app);
 
 server.listen(port , hostname , () =>{
-    console.log(`Server is listening http://${hostname}:${port}`);
+    console.log(`Server is listening at http://${hostname}:${port}`);
 })
